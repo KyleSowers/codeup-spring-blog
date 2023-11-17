f40ece3554a1435a973b8492281e8e2216762072
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-11-17 17:44:53 -0600

    Completed the Views exercise of dice rolling. Then created the Post class for the next part of exercise.

---
+++ src/main/java/com/codeup/codeupspringblog/DiceController.java
@@ -0,0 +1,30 @@
+package com.codeup.codeupspringblog;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
+public class DiceController {
+
+    @GetMapping("/roll-dice")
+    public String diceSelector() {
+        return "roll-dice";
+    }
+
+    @GetMapping("/roll-dice/{n}")
+    public  String diceResult(@PathVariable int n, Model model) {
+        int result = (int) (Math.floor(Math.random() * 6) + 1);
+        if (result == n) {
+            String correctGuess = "You guessed " + n + " correctly!";
+            model.addAttribute("message", correctGuess);
+        } else {
+            String incorrectGuess = "You guessed incorrectly, as " + result + " was the correct dice roll";
+            model.addAttribute("message", incorrectGuess);
+        }
+        return "diceResult";
+    }
+}
+
+
