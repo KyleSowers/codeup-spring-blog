74f4c1848b91a692df0cb82a127b072e549af81f
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-11-20 11:08:10 -0600

    finished Spring view exercises. PostController file now passes objects to the view for an individual post and all posts.

--- src/main/java/com/codeup/codeupspringblog/Post.java
+++ src/main/java/com/codeup/codeupspringblog/Post.java
@@ -1,11 +1,37 @@
 package com.codeup.codeupspringblog;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+
 public class Post {
+
+    private long id;
+
     private String title;
 
     private String body;
 
     public Post(){
+    }
+
+    public Post(long id, String title, String body) {
+       this.id = id;
+        this.title = title;
+        this.body = body;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public Post(long id) {
+        this.id = id;
     }
 
     public String getTitle() {
--- src/main/java/com/codeup/codeupspringblog/PostController.java
+++ src/main/java/com/codeup/codeupspringblog/PostController.java
@@ -1,24 +1,44 @@
 package com.codeup.codeupspringblog;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class PostController {
 
-    @RequestMapping(path = "/post", method = RequestMethod.GET)
-    @ResponseBody
-    public String post() {
-        return "Posts index page";
+//    @RequestMapping(path = "/post", method = RequestMethod.GET)
+//    @ResponseBody
+//    public String post() {
+//        return "Posts index page";
+//    }
+
+    @GetMapping("/post")
+    public String post(Model model) {
+        Post post1 = new Post(1, "First Post", "This is the first post.");
+        Post post2 = new Post(2, "Second Post", "This is the second post.");
+        List<Post> posts = new ArrayList<>();
+        posts.add(post1);
+        posts.add(post2);
+        model.addAttribute("posts", posts);
+        return "posts/index";
     }
 
-    @RequestMapping(path = "/post/{id}", method = RequestMethod.GET)
-    @ResponseBody
-    public String individualPost(@PathVariable String id) {
-        return "View an individual post";
+//    @RequestMapping(path = "/post/{id}", method = RequestMethod.GET)
+//    @ResponseBody
+//    public String individualPost(@PathVariable String id) {
+//        return "View an individual post";
+//    }
+
+//    Part 3 of exercise
+    @GetMapping("/post/{id}")
+    public String postId(Model model, @PathVariable int id) {
+        Post post = new Post(1, "Single Post", "This is the context of the new post.");
+        model.addAttribute("post", post);
+        return "posts/show";
     }
 
     @RequestMapping(path = "/post/create", method = RequestMethod.GET)
@@ -32,4 +52,6 @@ public class PostController {
     public String createPost() {
         return "Create a new post";
     }
+
+
 }
